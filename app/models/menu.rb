97c8d97c96eb408e996b56092c5376008530833c
Max Philips <maximilien.philips@gmail.com>
2016-08-22 17:34:21 +0100

    validation listv2 for menu-controller

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,5 +2,4 @@ class Menu < ApplicationRecord
   belongs_to :user
   validates :name, :photo, :price, :preparation_time, :min_guests, :max_guests, :category, presence: true
   validates :category, inclusion: { in: %w(Italian, Chinese, Japanese, Belgian, French, Indian, Lebanese, Persian, Italian, Etiopian, Vietnameese, English, Spanish, Carribean, Greek)}
-  validates
 end
