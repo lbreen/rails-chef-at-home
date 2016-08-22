e73ddd2c14fcd8c036d82dc55907fa3bedf5d020
disha patel <disha.p@hotmail.co.uk>
2016-08-22 18:13:31 +0100

    remove add rating to model migration

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,4 @@ class Booking < ApplicationRecord
   belongs_to :menu
   validates :user, :menu, :location, :time, :guests, presence:true
   validates :guests, inclusion: { in: %w(5..10)}
-  validates :rating, inclusion: {in: %w(5..10)}
 end
--- db/migrate/20160822164926_add_rating_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class AddRatingToBookings < ActiveRecord::Migration[5.0]
-  def change
-    add_column :bookings, :rating, :integer
-  end
-end
