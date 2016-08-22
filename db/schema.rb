ee3cbbb84008d05c01a9f669b83c2f1ad65e1b8b
Luke Breen <l.breen1@outlook.com>
2016-08-22 18:38:05 +0100

    Fixed DB issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160822170210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "menu_id"
+    t.string   "location"
+    t.date     "time"
+    t.integer  "guests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["menu_id"], name: "index_bookings_on_menu_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "photo"
+    t.integer  "price"
+    t.integer  "preparation_time"
+    t.integer  "min_guests"
+    t.integer  "max_guests"
+    t.text     "starter"
+    t.text     "main"
+    t.text     "dessert"
+    t.string   "category"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["user_id"], name: "index_menus_on_user_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email_address"
+    t.string   "postal_address"
+    t.integer  "rating"
+    t.text     "description"
+    t.boolean  "admin"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  add_foreign_key "bookings", "menus"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "menus", "users"
 end
