6ca816a6027b64ac9282eb4a018513151d274fd5
disha patel <disha.p@hotmail.co.uk>
2016-08-22 18:29:46 +0100

    bookings controller stuff done now

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :menu
+  validates :user, :menu, :location, :time, :guests, presence:true
+  validates :guests, inclusion: {in: %w(1..5)}
+  validates :rating, inclusion: {in: %w(1..5)}
 end
