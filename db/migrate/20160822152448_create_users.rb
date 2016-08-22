fa622fc0e8c9e4cc5c824cb602c439714bbf9372
Luke Breen <l.breen1@outlook.com>
2016-08-22 16:30:27 +0100

    Initialised gems and models

---
+++ db/migrate/20160822152448_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email_address
+      t.string :email_address
+      t.string :encrypted_password
+      t.integer :rating
+      t.text :description
+      t.boolean :admin
+
+      t.timestamps null: false
+    end
+  end
+end
