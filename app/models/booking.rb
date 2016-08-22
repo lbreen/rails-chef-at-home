ed959a360f3ee9cc28cc3d0861a826ebda97995f
disha patel <disha.p@hotmail.co.uk>
2016-08-22 17:08:05 +0100

    validation refatored

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,9 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :menu
-  validates :user, presence:true
-  validates :menu, presence:true
-  validates :location, presence:true
-  validates :time, presence:true
-  validates :guests, presence:true
+  validates :user, :menu, :location, :time, :guests, presence:true
 end
