f610127a4dbc4ab586e9538ef3b17c895acab149
Max Philips <maximilien.philips@gmail.com>
2016-08-22 17:23:50 +0100

    add other validations with categories

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,6 @@
 class Menu < ApplicationRecord
   belongs_to :user
-  validates :name presence: true
+  validates :name, :photo, :price, :preparation_time, :min_guests, :max_guests, :category, presence: true, uniqueness: true
+  validates :category, inclusion: { in: %w(Italian, Chinese, Japanese, Belgian, French, Indian, Lebanese, Persian, Italian, Etiopian, Vietnameese, English, Spanish, Carribean, Greek)}
+  validates
 end
