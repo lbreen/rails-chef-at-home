576c1193d8a5998eed620ef7909fe677f873f654
disha patel <disha.p@hotmail.co.uk>
2016-08-22 17:23:00 +0100

    added inclusion validation for guests

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :menu
   validates :user, :menu, :location, :time, :guests, presence:true
+  validates :guests, inclusion: { in: %w(5..10)}
 end
