d468353616cbc835f51698653c2405040da72bab
disha patel <disha.p@hotmail.co.uk>
2016-08-22 17:03:46 +0100

    all presence validations added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :menu
   validates :user, presence:true
+  validates :menu, presence:true
+  validates :location, presence:true
+  validates :time, presence:true
+  validates :guests, presence:true
 end
