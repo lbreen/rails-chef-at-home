95eeb3f63c5e9acb2f55fe7e3d61530cab524627
Luke Breen <l.breen1@outlook.com>
2016-08-22 18:52:18 +0100

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, :last_name, presence: true
+  validates :email_address, :postal_address, presence: true, uniqueness: true
+  validates :description, presence: true, length: { in: 0..150 }
+  has_many :bookings, :menus
 end
