051ab93afaa832f5d129d72bc938946eff09553e
Luke Breen <l.breen1@outlook.com>
2016-08-23 13:33:58 +0100

    Created seeds for user, menu and bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,70 +7,76 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+require 'devise'
 
-Booking.destroy_all
-User.destroy_all
-Menu.destroy_all
+# Booking.destroy_all
+# User.destroy_all
+# Menu.destroy_all
 
 users = []
 menus = []
-category = %w(Italian Chinese Japanese Belgian French Indian Lebanese Persian Italian Etiopian Vietnameese English Spanish Carribean Greek)}
+category = %w(Italian Chinese Japanese Belgian French Indian Lebanese Persian Italian Etiopian Vietnameese English Spanish Carribean Greek)
 
 10.times do
-  users << User.new({
-    first_name: Faker::Name.first_name ,
-    last_name: Faker::Name.last_name ,
+  users << User.create!({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
     postal_address: Faker::Address.street_address,
-    description: Faker::Lorem.paragraph ,
+    description: "Enter description here",
     admin: false,
-    email: Faker::Internet.free_email ,
+    email: Faker::Internet.free_email,
+    password: Devise.friendly_token.first(8)
     })
-  User.save
 end
 
+i = 0
+
 10.times do
-  i = 0
-  menus << Menu.new({
-    user_id: users[i]
+  menus << Menu.create!({
+    user_id: users[i].id,
     name: Faker::Lorem.sentence,
-    photo: Faker::Placeholdit.image ,
+    photo: Faker::Placeholdit.image,
     price: rand(5..20),
     preparation_time: rand(20..60),
-    min_guests: (1..4),
-    max_guests: (5..10),
+    min_guests: rand(1..4),
+    max_guests: rand(5..10),
     starter: Faker::Lorem.paragraph,
     main: Faker::Lorem.paragraph,
     dessert: Faker::Lorem.paragraph,
     category: category[i],
     })
-  Menu.save
   i += 1
 end
 
 #  Future bookings
 
-10.times do
-  i = 0
-  Booking.new({
-    user_id: users[i],
-    menu_id: menus[i],
-    location: Faker::Address.street_address,
+i = 0
+
+5.times do
+  Booking.create!({
+    user_id: users[i].id,
+    menu_id: menus[i].id,
+    location: users[i].postal_address,
     time: Faker::Time.forward(14, :evening),
-    guests: rand(1..10),
-    rating:
+    guests: rand(0..10),
     })
+
+  i += 1
 end
 
-#  Previous bookings
+#  Past bookings
 
-10.times do
-  i = 0
-  Booking.new({
-    user_id: users[i],
-    menu_id: menus[i],
-    location: Faker::Address.street_address,
-    time: Faker::Time.forward(14, :evening),
-    guests: rand(1..10),
+i = 0
+
+5.times do
+  Booking.create!({
+    user_id: users[i + 5].id,
+    menu_id: menus[i + 5].id,
+    location: users[i].postal_address,
+    time: Faker::Time.backward(14, :evening),
+    guests: rand(0..10),
     rating: rand(0..5)
     })
+
+  i += 1
 end
