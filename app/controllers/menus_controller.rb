64c4a45c260001b5ff76c6e6a974f8169e162972
Max Philips <maximilien.philips@gmail.com>
2016-08-23 15:26:25 +0100

    feature #show

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,16 @@
 class MenusController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_menu, only: [:show]
   def index
     @menus = Menu.all
   end
+
+  def show
+  end
+
+
+  def set_menu
+    @menu = Menu.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :menus, only: [:index]
+  resources :menus, only: [:index, :show]
 
   resources :users, only: [:index, :show]
 
