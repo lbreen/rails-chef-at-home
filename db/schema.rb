e92f9f7f585ecc47e559fe38d95ac10f3f3651dd
disha patel <disha.p@hotmail.co.uk>
2016-08-23 10:43:39 +0100

    bookings model updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822170210) do
+ActiveRecord::Schema.define(version: 20160823093704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160822170210) do
     t.integer  "guests"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "rating"
     t.index ["menu_id"], name: "index_bookings_on_menu_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
