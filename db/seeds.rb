c29cfc7683bbb92b5766b3c8e72b6692fec5443b
Luke Breen <l.breen1@outlook.com>
2016-08-23 12:29:50 +0100

    Created seeds for user, menu and booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,72 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Booking.destroy_all
+User.destroy_all
+Menu.destroy_all
+
+users = []
+menus = []
+category = %w(Italian Chinese Japanese Belgian French Indian Lebanese Persian Italian Etiopian Vietnameese English Spanish Carribean Greek)}
+
+10.times do
+  users << User.new({
+    first_name: Faker::Name.first_name ,
+    last_name: Faker::Name.last_name ,
+    postal_address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph ,
+    admin: false,
+    email: Faker::Internet.free_email ,
+    })
+  User.save
+end
+
+10.times do
+  i = 0
+  menus << Menu.new({
+    user_id: users[i]
+    name: Faker::Lorem.sentence,
+    photo: Faker::Placeholdit.image ,
+    price: rand(5..20),
+    preparation_time: rand(20..60),
+    min_guests: (1..4),
+    max_guests: (5..10),
+    starter: Faker::Lorem.paragraph,
+    main: Faker::Lorem.paragraph,
+    dessert: Faker::Lorem.paragraph,
+    category: category[i],
+    })
+  Menu.save
+  i += 1
+end
+
+#  Future bookings
+
+10.times do
+  i = 0
+  Booking.new({
+    user_id: users[i],
+    menu_id: menus[i],
+    location: Faker::Address.street_address,
+    time: Faker::Time.forward(14, :evening),
+    guests: rand(1..10),
+    rating:
+    })
+end
+
+#  Previous bookings
+
+10.times do
+  i = 0
+  Booking.new({
+    user_id: users[i],
+    menu_id: menus[i],
+    location: Faker::Address.street_address,
+    time: Faker::Time.forward(14, :evening),
+    guests: rand(1..10),
+    rating: rand(0..5)
+    })
+end
