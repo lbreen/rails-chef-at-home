9b4d3f3d8d9cdb3add26b85ddf1600dc0959d0aa
Luke Breen <l.breen1@outlook.com>
2016-08-23 15:53:20 +0100

    Added comments to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  # validates :description, presence: true, length: { in: 0..150 }
   has_many :bookings
   has_many :menus
+
+  #  These validations are not needed as we do not want to require them on sign up
+  # validates :first_name, :last_name, presence: true
+  # validates :description, presence: true, length: { in: 0..150 }
 end
