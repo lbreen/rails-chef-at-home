0c76282e3de91f9d75f81e645f5a1a9428a1c475
Luke Breen <l.breen1@outlook.com>
2016-08-23 10:17:51 +0100

    CHnaged has_many on to two lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
   validates :email_address, :postal_address, presence: true, uniqueness: true
   validates :description, presence: true, length: { in: 0..150 }
-  has_many :bookings, :menus
+  has_many :bookings
+  has_many :menus
 end
