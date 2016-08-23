8156b7f196ab2c61cc195c75c37117e7d2d70162
disha patel <disha.p@hotmail.co.uk>
2016-08-23 10:50:10 +0100

    created bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+
+  get 'bookings/new'
+
+  get 'bookings/create'
+
+  get 'bookings/show'
+
+  get 'bookings/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
