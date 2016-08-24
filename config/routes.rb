1eb2409990c2c0d1a855c6eae8f54fa190e68739
Luke Breen <l.breen1@outlook.com>
2016-08-24 16:54:54 +0100

    Added menu#new and menu#show routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     resources :bookings, only:[ :index ]
   end
 
-  resources :menus, only: [:index, :show] do
+  resources :menus, only: [:index, :show, :new, :create] do
     resources :bookings, only: [ :new, :create ]
   end
 
