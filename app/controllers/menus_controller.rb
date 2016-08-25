10efe04e05d33bf26ee9195dc7cdb5210ec18e67
Luke Breen <l.breen1@outlook.com>
2016-08-25 15:57:39 +0100

    Added edit menu form

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -51,6 +51,6 @@ class MenusController < ApplicationController
   end
 
   def menu_params
-    params.require(:menu).permit(:user_id, :name, :photo, :price, :preparation_time, :min_guests, :max_guests, :starter, :main, :dessert, :category, photos: [])
+    params.require(:menu).permit(:user_id, :name, :price, :preparation_time, :min_guests, :max_guests, :starter, :main, :dessert, :category, photos: [])
   end
 end
