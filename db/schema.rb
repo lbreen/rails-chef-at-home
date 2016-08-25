ba66e091c94b2c3c37e397df4cf698eb959ccaa1
Luke Breen <l.breen1@outlook.com>
2016-08-25 14:56:40 +0100

    Added meal photos as logos. Removed photo column from menu model (not needed as using attachinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825103236) do
+ActiveRecord::Schema.define(version: 20160825135120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20160825103236) do
   create_table "menus", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "name"
-    t.string   "photo"
     t.integer  "price"
     t.integer  "preparation_time"
     t.integer  "min_guests"
