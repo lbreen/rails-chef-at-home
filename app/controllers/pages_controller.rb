7a442cd85ef1991e812c7781fa6e6bcffde8adf8
Max Philips <maximilien.philips@gmail.com>
2016-08-26 15:15:59 +0100

    listing category

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: :home
 
+
   def home
+    @categories = %w(Italian Chinese Japanese Belgian French Indian Lebanese Persian Etiopian Vietnameese English Spanish Carribean Greek)
   end
 end
