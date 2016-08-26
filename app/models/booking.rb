e9fe442c8eb04f763a91fac7e74ca70b83e60318
disha patel <disha.p@hotmail.co.uk>
2016-08-26 15:24:06 +0100

    added restrictions to the booking form and made it a bit prettier

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,17 @@ class Booking < ApplicationRecord
   validates :location, presence: true
   validates :datetime, presence: true
   validates :guests, presence: true, numericality: true
+  validate :booking_date_cannot_be_in_the_past
   geocoded_by :location
   after_validation :geocode, if: :location_changed?
 
+
+
+
+  def booking_date_cannot_be_in_the_past
+    if datetime < Date.today
+      errors.add(:datetime, "Cannot be in the past")
+    end
+  end
 end
+
