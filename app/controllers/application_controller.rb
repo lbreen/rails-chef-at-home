b06f23e3549a72f73334e6471ac70d384ee8e287
Luke Breen <l.breen1@outlook.com>
2016-08-26 12:07:18 +0100

    Updated device strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :postal_address, :photo, :description])
   end
 end
